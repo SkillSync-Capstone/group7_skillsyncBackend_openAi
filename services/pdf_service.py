fe440d1853ecc021224bdcc86a126fb2509b200a
kevinPatel11 <kevinpatel1142@gmail.com>
2024-04-18 19:59:08 -0400

    revert

--- services/pdf_service.py
+++ services/pdf_service.py
@@ -1,7 +1,7 @@
-import os
-import boto3
 import PyPDF2
-from io import BytesIO
+import os
+
+TEXT_FILES_DIRECTORY = "text_files/"
 
 def get_file_size(file):
     file.seek(0, os.SEEK_END)
@@ -9,39 +9,27 @@ def get_file_size(file):
     file.seek(0)
     return size
 
-def upload_text_to_s3(pdf_file, filename, bucket_name):
+def extract_text_and_create_text_file(pdf_file, text_filename):
+    text = ''
     pdf_file.seek(0)
     pdf_reader = PyPDF2.PdfReader(pdf_file)
-    text_content = ''
-    for page in pdf_reader.pages:
-        page_text = page.extract_text()
-        if page_text:
-            text_content += page_text + '\n'
+    with open(os.path.join(TEXT_FILES_DIRECTORY, text_filename), 'w', encoding='utf-8') as text_file:
+        for page in pdf_reader.pages:
+            page_text = page.extract_text()
+            if page_text:  # Check if text is not None
+                text_file.write(page_text + '\n')
+    pdf_file.seek(0)  # Reset the PDF file position if needed
 
-    # Create a S3 client
-    s3 = boto3.client('s3')
-    object_name = f"text_files/{filename}.txt"
-    # Upload the file
-    s3.put_object(Body=text_content, Bucket=bucket_name, Key=object_name)
-    return f"File uploaded to S3 with key {object_name}"
-
-def upload_file(file, filename, bucket_name):
-    
-    # Check the split operation
-    base_filename = os.path.splitext(filename)[0]
-    
+def upload_file(file, filename):
     file_size = get_file_size(file)
-    
-    # Ensure this function is properly handling its inputs
-    confirmation_message = upload_text_to_s3(file, base_filename, bucket_name)
-    
-    # Check final URI formation
-    s3_uri = f"s3://{bucket_name}/text_files/{base_filename}.txt"
+    text_filename = f"{os.path.splitext(filename)[0]}.txt"
+    extract_text_and_create_text_file(file, text_filename)
     
     file_details = {
         "Filename": filename,
         "File size": file_size,
-        "S3 URI": s3_uri
+        "Text File": os.path.join(TEXT_FILES_DIRECTORY, text_filename)
     }
     
+    confirmation_message = f"File '{filename}' processed successfully and saved as '{text_filename}' in the specified directory."
     return {"message": confirmation_message, "file_details": file_details}
